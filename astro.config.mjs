6077af447e36599855d5a0b54fb3af77c0606f09
Alejandro Serna <aserna37@hotmail.com>
2024-11-23 13:30:20 -0500

    Validando error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import db from "@astrojs/db";
 // import node from "@astrojs/node";
 import cloudflare from "@astrojs/cloudflare";
 
 
 // https://astro.build/config
-export default defineConfig( {
+export default defineConfig({
   site: 'https://example.com',
-  integrations: [ mdx(), sitemap() ],
+  integrations: [mdx(), sitemap(), db()],
   output: "hybrid",
   adapter: cloudflare()
-} );
+});
